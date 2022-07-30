e9e3e6010b4fa6938714a0cbfe46903534ea1a26
Fabio Araujo <fabio.re@edu.unifor.br>
2022-07-29 21:23:15 -0300

    Testes de login e registro do Alurapic

--- Cypress/Projeto_Alura/cypress/integration/3-Alurapic/alurapic.spec.js
+++ Cypress/Projeto_Alura/cypress/integration/3-Alurapic/alurapic.spec.js
@@ -4,14 +4,57 @@ describe('Login e registro de usuarios alurapic', () => {
         cy.visit('https://alura-fotos.herokuapp.com');
     })
 
-    it('verificar mensagens validacao', () => {
+     it('verificar mensagens validacao', () => {
+         cy.contains('a', 'Register now').click();
+         cy.contains('button', 'Register').click();
+         cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+         cy.contains('button', 'Register').click();
+         cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+     })
+
+     it('saber se o email é invalido', () => {
+         cy.contains('a', 'Register now').click();
+         cy.contains('button', 'Register').click();
+         cy.get('input[formcontrolname="email"]', ).type('Fábio');
+         cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+        
+     })
+
+     it('saber se o username é invalido', () => {
+         cy.contains('a', 'Register now').click();
+         cy.contains('button', 'Register').click();
+         cy.get('input[formcontrolname="userName"]', ).type('Pepsi');
+         cy.contains('button', 'Register').click();
+         cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
+                
+     })
+
+    it('saber se o username é valido', () => {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
-        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
-        cy.contains('button', 'Register').click();
-        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
-        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
-        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+        cy.get('input[formcontrolname="userName"]', ).type('pepsis');
+        cy.contains('small', 'User available').should('be.visible');
+        
     })
+
+     it('saber se o username ja foi usado', () => {
+         cy.contains('a', 'Register now').click();
+         cy.contains('button', 'Register').click();
+         cy.get('input[formcontrolname="userName"]', ).type('pepsi');
+         cy.contains('button', 'Register').click();
+         cy.contains('ap-vmessage', 'Username already taken').should('be.visible');
+        
+     })
+
+     it('verifica mensagens de senha com menos de 8 caracteres', () => {
+         cy.contains('a', 'Register now').click();
+         cy.contains('button', 'Register').click();
+         cy.get('input[formcontrolname="password"]', ).type('123');
+         cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+                
+     })
 
 })
